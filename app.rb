5a139d3c89717538f3c509ec8faa8413a1b04229
polinagusakova <poll.gusakova@gmail.com>
2018-10-05 22:53:47 +0300

    Create product migration

--- app.rb
+++ app.rb
@@ -2,13 +2,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra-activerecord'
+require 'sinatra/activerecord'
 
 set :database, "sqlite3:pizzashop.db"
 
-class Product < ActiveRecord::Base
-
-end
+class Product < ActiveRecord::Base; end
 
 get '/' do
 	erb :index
