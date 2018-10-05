61fc8a8df2f521138ababaa9f8f1f658f92a8de9
polinagusakova <poll.gusakova@gmail.com>
2018-10-05 23:20:15 +0300

    Update product migration with new values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_194254) do
+ActiveRecord::Schema.define(version: 2018_10_05_195903) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
