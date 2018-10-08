7e395bb300c2c904639f8a67ef51db30fc6517e5
polinagusakova <poll.gusakova@gmail.com>
2018-10-08 22:58:31 +0300

    find_item function to sum all selected items

--- app.rb
+++ app.rb
@@ -16,3 +16,4 @@ end
 get '/about' do
 	erb :about
 end
+
--- public/app.js
+++ public/app.js
@@ -15,5 +15,18 @@ function add_to_cart(id)
 
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
-	window.localStorage.setItem(key, x);
+	window.localStorage.setItem(key, x);	
+}
+
+
+function find_items() 
+{
+	var total = 0;
+
+	for (var i = 0; i < localStorage.length; i++) {
+
+		var value = localStorage.getItem(localStorage.key(i));
+
+    	total += value * 1;
+	}
 }
