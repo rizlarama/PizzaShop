07b8fd74783d4f0e132095b23d9fa893c0408125
polinagusakova <poll.gusakova@gmail.com>
2018-10-10 22:46:16 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -40,3 +40,24 @@ function cart_get_number_of_items()
 	
 	return total; // can check in console in browser (cart_get_number_of_items())
 }
+
+
+
+function cart_get_orders() 
+{
+	var orders ='';
+
+	for (var i = 0; i < window.localStorage.length; i++) {
+
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if (key.indexOf('product_') == 0)
+		{	
+			orders = orders + key + '=' + value + ',';
+		}
+    	
+	}
+	
+	return orders; // can check in console in browser (cart_get_number_of_items())
+}
